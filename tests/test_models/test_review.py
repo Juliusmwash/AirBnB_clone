a120e9f8115cc739e8511ae7a247bff505e5a5f0
Abbah Divine <abbahattamah@gmail.com>
2023-05-13 02:17:09 +0100

    tests

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''Tests User class'''
+
+import models
+from models.base_model import BaseModel
+from models.review import Review
+import os
+import unittest
+
+
+class TestReview(unittest.TestCase):
+    '''start tests'''
+
+    def test_docstring(self):
+        '''test for if funcions, methods, classes
+        and modules have docstring'''
+        msj = "Módulo does not has docstring"
+        self.assertIsNotNone(models.review.__doc__, msj)  # Modules
+        msj = "Clase does not has docstring"
+        self.assertIsNotNone(Review.__doc__, msj)  # Classes
+
+    def test_executable_file(self):
+        '''test fof if file has permissions u+x to execute'''
+        # Check for read access
+        is_read_true = os.access('models/review.py', os.R_OK)
+        self.assertTrue(is_read_true)
+        # Check for write access
+        is_write_true = os.access('models/review.py', os.W_OK)
+        self.assertTrue(is_write_true)
+        # Check for execution access
+        is_exec_true = os.access('models/review.py', os.X_OK)
+        self.assertTrue(is_exec_true)
+
+    def test_is_an_instance(self):
+        '''check for if my_model is an instance of BaseModel'''
+        my_review = Review()
+        self.assertIsInstance(my_review, Review)
